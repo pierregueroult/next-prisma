f35902f0244aea1e35dc6bcba3ccf3dc6c979387
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-04-22 14:37:18 +0200

    feat: add e2e test fixture with basic nextjs app

---
+++ e2e/fixture/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+import { withNextPrisma } from "@pglabs/next-prisma";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withNextPrisma(nextConfig);
